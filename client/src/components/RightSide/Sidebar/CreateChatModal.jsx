58325a079428501545a8d737f7d99daf40558be8
impulse05 <a.gupta335001@gmail.com>
2024-03-24 16:52:18 +0530

    Modal fix

--- client/src/components/RightSide/Sidebar/CreateChatModal.jsx
+++ client/src/components/RightSide/Sidebar/CreateChatModal.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getUsers } from '../../api/users';
-
+import { ToastContainer, toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 import { useChat } from '../../../Context/chatProvider';
 import { CreateChat } from '../../api/chats';
@@ -17,7 +17,7 @@ export default function CreateChatModal({ isOpen, onClose }) {
             setAllUsers(data);
         })
     }, []);
-    const {setRefresh,setSelectedChat,user} = useChat();
+    const { setRefresh, setSelectedChat, user } = useChat();
     // Function to handle toggling the checkbox state
     const toggleCheckbox = (userId) => {
         console.log(userId);
@@ -29,19 +29,19 @@ export default function CreateChatModal({ isOpen, onClose }) {
     };
 
     const handleCreateChat = () => {
-        if(checkedUsers.length>1 && !groupName) {
-            return alert("Enter the group name");
+        if (checkedUsers.length > 1 && !groupName) {
+            return toast("Enter the group name");
         }
 
-        CreateChat({isGroupChat:checkedUsers.length>1, users: checkedUsers, chatName: groupName})
-        .then(data => {
-            setRefresh(prev => !prev);
-            setCheckedUsers([]);
-            setGroupName("");
-            setSelectedChat(data);
-            navigate(`/chats/${data._id}`);
-            onClose();
-        }) 
+        CreateChat({ isGroupChat: checkedUsers.length > 1, users: checkedUsers, chatName: groupName })
+            .then(data => {
+                setRefresh(prev => !prev);
+                setCheckedUsers([]);
+                setGroupName("");
+                setSelectedChat(data);
+                navigate(`/chats/${data._id}`);
+                onClose();
+            })
     };
 
     return (
@@ -49,17 +49,22 @@ export default function CreateChatModal({ isOpen, onClose }) {
             <div className="fixed inset-0 bg-black opacity-50"></div>
             <div className="bg-[#1A202C] w-50 overflow-hidden shadow-xl transform transition-all max-w-3xl mx-auto">
                 <div className="p-6">
-                    
-                    {checkedUsers.length > 1 && 
-                    
-                    <>
-                    <h2 className="text-lg font-semibold mb-4">Enter Group name:</h2>
-                        <input onChange={(e) => { setGroupName(e.target.value) }} type="text" className=" transform hover:scale-105 duration-100 transition-transform  mb-4 rounded p-4 flex text-black" placeholder="Enter Group Name" />
-                    </>
+
+                    {checkedUsers.length > 1 &&
+
+                        <>
+                            <h2 className="text-lg font-semibold mb-4">Enter Group name:</h2>
+                            <input
+                                onChange={(e) => { setGroupName(e.target.value) }}
+                                type="text"
+                                className="transform hover:scale-105 duration-100 transition-transform mb-4 rounded p-4 flex text-black border border-gray-300 focus:outline-none focus:border-blue-500"
+                                placeholder="Enter Group Name"
+                            />
+                        </>
                     }
 
                     <h2 className="text-lg font-semibold mb-4">Select Users</h2>
-                    <div>
+                    <div className="h-72 overflow-y-auto">
                         {allUsers?.map(user => (
                             <div key={user._id} onClick={() => toggleCheckbox(user._id)} className={`entry cursor-pointer transform hover:scale-105 duration-100 transition-transform  mb-4 rounded p-4 flex hover:bg-[#2D3748]`}>
                                 <div className="flex-2">
@@ -75,8 +80,8 @@ export default function CreateChatModal({ isOpen, onClose }) {
                         ))}
                     </div>
                     <div className="flex justify-end mt-6">
-                      
-                        <button onClick={()=>{
+
+                        <button onClick={() => {
                             onClose();
                             setCheckedUsers([]);
                             setGroupName("");
